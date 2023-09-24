1047ff406efbb201bf72b8eb1305b7d0dbc31b9d
florxyz <florgxyz@gmail.com>
2023-09-24 10:30:49 -0600

    Números Form

---
+++ ProyectoApp/Números.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace ProyectoApp
+{
+    partial class Números
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            button11 = new Button();
+            button12 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Sigmar One", 26.2499962F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.Location = new Point(99, 62);
+            button1.Name = "button1";
+            button1.Size = new Size(97, 88);
+            button1.TabIndex = 0;
+            button1.Text = "0";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Sigmar One", 26.2499962F, FontStyle.Bold, GraphicsUnit.Point);
+            button2.Location = new Point(269, 62);
+            button2.Name = "button2";
+            button2.Size = new Size(97, 88);
+            button2.TabIndex = 1;
+            button2.Text = "1";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Sigmar One", 26.2499962F, FontStyle.Bold, GraphicsUnit.Point);
+            button3.Location = new Point(441, 62);
+            button3.Name = "button3";
+            button3.Size = new Size(97, 88);
+            button3.TabIndex = 2;
+            button3.Text = "2";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Font = new Font("Sigmar One", 26.2499962F, FontStyle.Bold, GraphicsUnit.Point);
+            button4.Location = new Point(604, 62);
+            button4.Name = "button4";
+            button4.Size = new Size(97, 88);
+            button4.TabIndex = 3;
+            button4.Text = "3";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Font = new Font("Sigmar One", 26.2499962F, FontStyle.Bold, GraphicsUnit.Point);
+            button5.Location = new Point(180, 182);
+            button5.Name = "button5";
+            button5.Size = new Size(97, 88);
+            button5.TabIndex = 4;
+            button5.Text = "4";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Font = new Font("Sigmar One", 26.2499962F, FontStyle.Bold, GraphicsUnit.Point);
+            button6.Location = new Point(367, 182);
+            button6.Name = "button6";
+            button6.Size = new Size(97, 88);
+            button6.TabIndex = 5;
+            button6.Text = "5";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            button7.Font = new Font("Sigmar One", 26.2499962F, FontStyle.Bold, GraphicsUnit.Point);
+            button7.Location = new Point(534, 182);
+            button7.Name = "button7";
+            button7.Size = new Size(97, 88);
+            button7.TabIndex = 6;
+            button7.Text = "6";
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            button8.Font = new Font("Sigmar One", 26.2499962F, FontStyle.Bold, GraphicsUnit.Point);
+            button8.Location = new Point(99, 321);
+            button8.Name = "button8";
+            button8.Size = new Size(97, 88);
+            button8.TabIndex = 7;
+            button8.Text = "7";
+            button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            button9.Font = new Font("Sigmar One", 26.2499962F, FontStyle.Bold, GraphicsUnit.Point);
+            button9.Location = new Point(269, 321);
+            button9.Name = "button9";
+            button9.Size = new Size(97, 88);
+            button9.TabIndex = 8;
+            button9.Text = "8";
+            button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            button10.Font = new Font("Sigmar One", 26.2499962F, FontStyle.Bold, GraphicsUnit.Point);
+            button10.Location = new Point(441, 321);
+            button10.Name = "button10";
+            button10.Size = new Size(97, 88);
+            button10.TabIndex = 9;
+            button10.Text = "9";
+            button10.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            button11.Font = new Font("Sigmar One", 26.2499962F, FontStyle.Bold, GraphicsUnit.Point);
+            button11.Location = new Point(604, 321);
+            button11.Name = "button11";
+            button11.Size = new Size(97, 88);
+            button11.TabIndex = 10;
+            button11.Text = "10";
+            button11.UseVisualStyleBackColor = true;
+            // 
+            // button12
+            // 
+            button12.BackColor = Color.FromArgb(192, 0, 0);
+            button12.FlatStyle = FlatStyle.Flat;
+            button12.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            button12.Location = new Point(12, 12);
+            button12.Name = "button12";
+            button12.Size = new Size(29, 23);
+            button12.TabIndex = 11;
+            button12.Text = "X";
+            button12.UseVisualStyleBackColor = false;
+            // 
+            // Números
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(255, 192, 192);
+            ClientSize = new Size(800, 450);
+            Controls.Add(button12);
+            Controls.Add(button11);
+            Controls.Add(button10);
+            Controls.Add(button9);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Números";
+            Text = "Números";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+        private Button button11;
+        private Button button12;
+    }
+}
