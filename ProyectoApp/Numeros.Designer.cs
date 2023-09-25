458030d5a9f66de90ddbe273747f95fe56982321
cam1la444 <camila24melara@gmail.com>
2023-09-25 15:05:22 -0600

    Agregar botones para numeros del 20 al 100

--- ProyectoApp/Numeros.Designer.cs
+++ ProyectoApp/Numeros.Designer.cs
@@ -39,7 +39,18 @@
             btnCero = new Button();
             btnDos = new Button();
             btnDiez = new Button();
-            btnNumClose = new Button();
+            button1 = new Button();
+            label1 = new Label();
+            btn100 = new Button();
+            btn90 = new Button();
+            btn60 = new Button();
+            btn80 = new Button();
+            btn70 = new Button();
+            btn50 = new Button();
+            btn40 = new Button();
+            btn30 = new Button();
+            btn20 = new Button();
+            btnVolver = new Button();
             SuspendLayout();
             // 
             // btnUno
@@ -47,9 +58,9 @@
             btnUno.BackColor = Color.FromArgb(255, 192, 192);
             btnUno.FlatStyle = FlatStyle.Popup;
             btnUno.Font = new Font("Comic Sans MS", 26F, FontStyle.Bold, GraphicsUnit.Point);
-            btnUno.Location = new Point(84, 165);
+            btnUno.Location = new Point(195, 92);
             btnUno.Name = "btnUno";
-            btnUno.Size = new Size(98, 89);
+            btnUno.Size = new Size(85, 63);
             btnUno.TabIndex = 0;
             btnUno.Text = "1";
             btnUno.UseVisualStyleBackColor = false;
@@ -61,9 +72,9 @@
             btnNueve.FlatStyle = FlatStyle.Popup;
             btnNueve.Font = new Font("Comic Sans MS", 26F, FontStyle.Bold, GraphicsUnit.Point);
             btnNueve.ForeColor = Color.Teal;
-            btnNueve.Location = new Point(513, 295);
+            btnNueve.Location = new Point(473, 191);
             btnNueve.Name = "btnNueve";
-            btnNueve.Size = new Size(98, 89);
+            btnNueve.Size = new Size(85, 63);
             btnNueve.TabIndex = 1;
             btnNueve.Text = "9";
             btnNueve.UseVisualStyleBackColor = false;
@@ -75,9 +86,9 @@
             btnOcho.FlatStyle = FlatStyle.Popup;
             btnOcho.Font = new Font("Comic Sans MS", 26F, FontStyle.Bold, GraphicsUnit.Point);
             btnOcho.ForeColor = Color.Teal;
-            btnOcho.Location = new Point(369, 295);
+            btnOcho.Location = new Point(332, 191);
             btnOcho.Name = "btnOcho";
-            btnOcho.Size = new Size(98, 89);
+            btnOcho.Size = new Size(85, 63);
             btnOcho.TabIndex = 2;
             btnOcho.Text = "8";
             btnOcho.UseVisualStyleBackColor = false;
@@ -89,9 +100,9 @@
             btnSiete.FlatStyle = FlatStyle.Popup;
             btnSiete.Font = new Font("Comic Sans MS", 26F, FontStyle.Bold, GraphicsUnit.Point);
             btnSiete.ForeColor = Color.Teal;
-            btnSiete.Location = new Point(226, 295);
+            btnSiete.Location = new Point(195, 191);
             btnSiete.Name = "btnSiete";
-            btnSiete.Size = new Size(98, 89);
+            btnSiete.Size = new Size(85, 63);
             btnSiete.TabIndex = 3;
             btnSiete.Text = "7";
             btnSiete.UseVisualStyleBackColor = false;
@@ -103,9 +114,9 @@
             btnSeis.FlatStyle = FlatStyle.Popup;
             btnSeis.Font = new Font("Comic Sans MS", 26F, FontStyle.Bold, GraphicsUnit.Point);
             btnSeis.ForeColor = Color.Teal;
-            btnSeis.Location = new Point(84, 295);
+            btnSeis.Location = new Point(57, 191);
             btnSeis.Name = "btnSeis";
-            btnSeis.Size = new Size(98, 89);
+            btnSeis.Size = new Size(85, 63);
             btnSeis.TabIndex = 4;
             btnSeis.Text = "6";
             btnSeis.UseVisualStyleBackColor = false;
@@ -116,9 +127,9 @@
             btnCinco.BackColor = Color.FromArgb(255, 192, 192);
             btnCinco.FlatStyle = FlatStyle.Popup;
             btnCinco.Font = new Font("Comic Sans MS", 26F, FontStyle.Bold, GraphicsUnit.Point);
-            btnCinco.Location = new Point(642, 165);
+            btnCinco.Location = new Point(752, 92);
             btnCinco.Name = "btnCinco";
-            btnCinco.Size = new Size(98, 89);
+            btnCinco.Size = new Size(85, 63);
             btnCinco.TabIndex = 5;
             btnCinco.Text = "5";
             btnCinco.UseVisualStyleBackColor = false;
@@ -129,9 +140,9 @@
             btnCuatro.BackColor = Color.FromArgb(255, 192, 192);
             btnCuatro.FlatStyle = FlatStyle.Popup;
             btnCuatro.Font = new Font("Comic Sans MS", 26F, FontStyle.Bold, GraphicsUnit.Point);
-            btnCuatro.Location = new Point(513, 165);
+            btnCuatro.Location = new Point(617, 92);
             btnCuatro.Name = "btnCuatro";
-            btnCuatro.Size = new Size(98, 89);
+            btnCuatro.Size = new Size(85, 63);
             btnCuatro.TabIndex = 6;
             btnCuatro.Text = "4";
             btnCuatro.UseVisualStyleBackColor = false;
@@ -142,9 +153,9 @@
             btnTres.BackColor = Color.FromArgb(255, 192, 192);
             btnTres.FlatStyle = FlatStyle.Popup;
             btnTres.Font = new Font("Comic Sans MS", 26F, FontStyle.Bold, GraphicsUnit.Point);
-            btnTres.Location = new Point(369, 165);
+            btnTres.Location = new Point(473, 92);
             btnTres.Name = "btnTres";
-            btnTres.Size = new Size(98, 89);
+            btnTres.Size = new Size(85, 63);
             btnTres.TabIndex = 7;
             btnTres.Text = "3";
             btnTres.UseVisualStyleBackColor = false;
@@ -152,13 +163,13 @@
             // 
             // btnCero
             // 
-            btnCero.BackColor = Color.RosyBrown;
+            btnCero.BackColor = Color.FromArgb(255, 192, 192);
             btnCero.FlatStyle = FlatStyle.Popup;
             btnCero.Font = new Font("Comic Sans MS", 26F, FontStyle.Bold, GraphicsUnit.Point);
             btnCero.ForeColor = Color.FromArgb(128, 64, 0);
-            btnCero.Location = new Point(369, 35);
+            btnCero.Location = new Point(57, 92);
             btnCero.Name = "btnCero";
-            btnCero.Size = new Size(98, 89);
+            btnCero.Size = new Size(85, 63);
             btnCero.TabIndex = 8;
             btnCero.Text = "0";
             btnCero.UseVisualStyleBackColor = false;
@@ -170,9 +181,9 @@
             btnDos.FlatStyle = FlatStyle.Popup;
             btnDos.Font = new Font("Comic Sans MS", 26F, FontStyle.Bold, GraphicsUnit.Point);
             btnDos.ForeColor = Color.Brown;
-            btnDos.Location = new Point(226, 165);
+            btnDos.Location = new Point(332, 92);
             btnDos.Name = "btnDos";
-            btnDos.Size = new Size(98, 89);
+            btnDos.Size = new Size(85, 63);
             btnDos.TabIndex = 9;
             btnDos.Text = "2";
             btnDos.UseVisualStyleBackColor = false;
@@ -184,33 +195,182 @@
             btnDiez.FlatStyle = FlatStyle.Popup;
             btnDiez.Font = new Font("Comic Sans MS", 26F, FontStyle.Bold, GraphicsUnit.Point);
             btnDiez.ForeColor = Color.Teal;
-            btnDiez.Location = new Point(642, 295);
+            btnDiez.Location = new Point(617, 191);
             btnDiez.Name = "btnDiez";
-            btnDiez.Size = new Size(98, 89);
+            btnDiez.Size = new Size(85, 63);
             btnDiez.TabIndex = 10;
             btnDiez.Text = "10";
             btnDiez.UseVisualStyleBackColor = false;
             btnDiez.Click += btnDiez_Click;
             // 
-            // btnNumClose
+            // button1
             // 
-            btnNumClose.BackColor = Color.Red;
-            btnNumClose.FlatStyle = FlatStyle.Flat;
-            btnNumClose.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
-            btnNumClose.ForeColor = SystemColors.ControlText;
-            btnNumClose.Location = new Point(12, 12);
-            btnNumClose.Name = "btnNumClose";
-            btnNumClose.Size = new Size(30, 27);
-            btnNumClose.TabIndex = 11;
-            btnNumClose.Text = "X";
-            btnNumClose.UseVisualStyleBackColor = false;
+            button1.BackColor = Color.Red;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.ForeColor = SystemColors.ControlText;
+            button1.Location = new Point(816, 12);
+            button1.Name = "button1";
+            button1.Size = new Size(30, 27);
+            button1.TabIndex = 12;
+            button1.Text = "X";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("HP Simplified", 26.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.ForeColor = Color.DarkSlateBlue;
+            label1.Location = new Point(304, 20);
+            label1.Name = "label1";
+            label1.Size = new Size(289, 39);
+            label1.TabIndex = 13;
+            label1.Text = "Lección Abecedario";
+            // 
+            // btn100
+            // 
+            btn100.BackColor = Color.FromArgb(255, 192, 255);
+            btn100.FlatStyle = FlatStyle.Popup;
+            btn100.Font = new Font("Comic Sans MS", 26F, FontStyle.Bold, GraphicsUnit.Point);
+            btn100.ForeColor = Color.Purple;
+            btn100.Location = new Point(473, 396);
+            btn100.Name = "btn100";
+            btn100.Size = new Size(100, 63);
+            btn100.TabIndex = 14;
+            btn100.Text = "100";
+            btn100.UseVisualStyleBackColor = false;
+            // 
+            // btn90
+            // 
+            btn90.BackColor = Color.FromArgb(255, 192, 255);
+            btn90.FlatStyle = FlatStyle.Popup;
+            btn90.Font = new Font("Comic Sans MS", 26F, FontStyle.Bold, GraphicsUnit.Point);
+            btn90.ForeColor = Color.Purple;
+            btn90.Location = new Point(332, 396);
+            btn90.Name = "btn90";
+            btn90.Size = new Size(85, 63);
+            btn90.TabIndex = 15;
+            btn90.Text = "90";
+            btn90.UseVisualStyleBackColor = false;
+            // 
+            // btn60
+            // 
+            btn60.BackColor = Color.FromArgb(255, 255, 192);
+            btn60.FlatStyle = FlatStyle.Popup;
+            btn60.Font = new Font("Comic Sans MS", 26F, FontStyle.Bold, GraphicsUnit.Point);
+            btn60.ForeColor = Color.FromArgb(192, 192, 0);
+            btn60.Location = new Point(473, 295);
+            btn60.Name = "btn60";
+            btn60.Size = new Size(85, 63);
+            btn60.TabIndex = 16;
+            btn60.Text = "60";
+            btn60.UseVisualStyleBackColor = false;
+            // 
+            // btn80
+            // 
+            btn80.BackColor = Color.FromArgb(255, 255, 192);
+            btn80.FlatStyle = FlatStyle.Popup;
+            btn80.Font = new Font("Comic Sans MS", 26F, FontStyle.Bold, GraphicsUnit.Point);
+            btn80.ForeColor = Color.FromArgb(192, 192, 0);
+            btn80.Location = new Point(752, 295);
+            btn80.Name = "btn80";
+            btn80.Size = new Size(85, 63);
+            btn80.TabIndex = 17;
+            btn80.Text = "80";
+            btn80.UseVisualStyleBackColor = false;
+            // 
+            // btn70
+            // 
+            btn70.BackColor = Color.FromArgb(255, 255, 192);
+            btn70.FlatStyle = FlatStyle.Popup;
+            btn70.Font = new Font("Comic Sans MS", 26F, FontStyle.Bold, GraphicsUnit.Point);
+            btn70.ForeColor = Color.FromArgb(192, 192, 0);
+            btn70.Location = new Point(617, 295);
+            btn70.Name = "btn70";
+            btn70.Size = new Size(85, 63);
+            btn70.TabIndex = 18;
+            btn70.Text = "70";
+            btn70.UseVisualStyleBackColor = false;
+            // 
+            // btn50
+            // 
+            btn50.BackColor = Color.FromArgb(255, 255, 192);
+            btn50.FlatStyle = FlatStyle.Popup;
+            btn50.Font = new Font("Comic Sans MS", 26F, FontStyle.Bold, GraphicsUnit.Point);
+            btn50.ForeColor = Color.FromArgb(192, 192, 0);
+            btn50.Location = new Point(332, 295);
+            btn50.Name = "btn50";
+            btn50.Size = new Size(85, 63);
+            btn50.TabIndex = 19;
+            btn50.Text = "50";
+            btn50.UseVisualStyleBackColor = false;
+            // 
+            // btn40
+            // 
+            btn40.BackColor = Color.FromArgb(255, 255, 192);
+            btn40.FlatStyle = FlatStyle.Popup;
+            btn40.Font = new Font("Comic Sans MS", 26F, FontStyle.Bold, GraphicsUnit.Point);
+            btn40.ForeColor = Color.FromArgb(192, 192, 0);
+            btn40.Location = new Point(195, 295);
+            btn40.Name = "btn40";
+            btn40.Size = new Size(85, 63);
+            btn40.TabIndex = 20;
+            btn40.Text = "40";
+            btn40.UseVisualStyleBackColor = false;
+            // 
+            // btn30
+            // 
+            btn30.BackColor = Color.FromArgb(255, 255, 192);
+            btn30.FlatStyle = FlatStyle.Popup;
+            btn30.Font = new Font("Comic Sans MS", 26F, FontStyle.Bold, GraphicsUnit.Point);
+            btn30.ForeColor = Color.FromArgb(192, 192, 0);
+            btn30.Location = new Point(57, 295);
+            btn30.Name = "btn30";
+            btn30.Size = new Size(85, 63);
+            btn30.TabIndex = 21;
+            btn30.Text = "30";
+            btn30.UseVisualStyleBackColor = false;
+            // 
+            // btn20
+            // 
+            btn20.BackColor = Color.FromArgb(240, 249, 248);
+            btn20.FlatStyle = FlatStyle.Popup;
+            btn20.Font = new Font("Comic Sans MS", 26F, FontStyle.Bold, GraphicsUnit.Point);
+            btn20.ForeColor = Color.Teal;
+            btn20.Location = new Point(752, 191);
+            btn20.Name = "btn20";
+            btn20.Size = new Size(85, 63);
+            btn20.TabIndex = 22;
+            btn20.Text = "20";
+            btn20.UseVisualStyleBackColor = false;
+            // 
+            // btnVolver
+            // 
+            btnVolver.Font = new Font("HP Simplified", 11.9999981F, FontStyle.Regular, GraphicsUnit.Point);
+            btnVolver.Location = new Point(745, 431);
+            btnVolver.Name = "btnVolver";
+            btnVolver.Size = new Size(83, 36);
+            btnVolver.TabIndex = 23;
+            btnVolver.Text = "Volver";
+            btnVolver.UseVisualStyleBackColor = true;
             // 
             // Numeros
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(btnNumClose);
+            ClientSize = new Size(895, 492);
+            Controls.Add(btnVolver);
+            Controls.Add(btn20);
+            Controls.Add(btn30);
+            Controls.Add(btn40);
+            Controls.Add(btn50);
+            Controls.Add(btn70);
+            Controls.Add(btn80);
+            Controls.Add(btn60);
+            Controls.Add(btn90);
+            Controls.Add(btn100);
+            Controls.Add(label1);
+            Controls.Add(button1);
             Controls.Add(btnDiez);
             Controls.Add(btnDos);
             Controls.Add(btnCero);
@@ -227,6 +387,7 @@
             Name = "Numeros";
             Text = "Numeros";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -242,6 +403,17 @@
         private Button btnCero;
         private Button btnDos;
         private Button btnDiez;
-        private Button btnNumClose;
+        private Button button1;
+        private Label label1;
+        private Button btn100;
+        private Button btn90;
+        private Button btn60;
+        private Button btn80;
+        private Button btn70;
+        private Button btn50;
+        private Button btn40;
+        private Button btn30;
+        private Button btn20;
+        private Button btnVolver;
     }
 }
